2cf44724cff1171e30c9c240ee58cc021ab05dae
Dmitry <royalmailforloner@gmail.com>
2022-06-14 03:48:10 +0300

    feat: complete function enableValidation

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,70 +1,74 @@
-// Показ ошибки ввода
-function showInputError (formElement, inputElement, errorMessage) {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add('popup__container-input_invalid');
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add('popup__container-input-error_visible');
-}
-
-// Скрытие ошибки ввода
-function hideInputError (formElement, inputElement) {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove('popup__container-input_invalid');
-  errorElement.classList.remove('popup__container-input-error_visible');
-  errorElement.textContent = '';
-}
-
-// Проверка валидности введённых данных
-function checkInputValidity (formElement, inputElement) {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement, inputElement);
-  }
-}
-
-// Проверка валидности всех полей формы
-function checkInvalidInputs (inputsList) {
-  return inputsList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  });
-}
-
-// Переключатель состояния кнопки отправки формы
-function toggleButtonState (inputsList, buttonElement) {
-  if (checkInvalidInputs(inputsList)) {
-    buttonElement.classList.add('popup__container-submit_disabled');
-    buttonElement.disabled = true;
-  } else {
-    buttonElement.classList.remove('popup__container-submit_disabled');
-    buttonElement.disabled = false;
-  }
-}
-
-// Добавление обработчиков всем полям формы
-function setEventListeners (formElement) {
-  const inputsList = Array.from(formElement.querySelectorAll('.popup__container-input'));
-  const buttonElement = formElement.querySelector('.popup__container-submit');
-
-  toggleButtonState(inputsList, buttonElement);
-
-  inputsList.forEach((inputElement) => {
-    inputElement.addEventListener('input', function () {
-      checkInputValidity(formElement, inputElement);
-      toggleButtonState(inputsList, buttonElement);
-    });
-  });
-}
-
 // Добавление обработчиков всем формам
-export function enableValidation () {
-  const formsList = Array.from(document.querySelectorAll('.popup__container'));
-  
+export function enableValidation(arr) {
+  const formsList = Array.from(document.querySelectorAll(arr["formSelector"]));
+
   formsList.forEach((formElement) => {
-    formElement.addEventListener('submit', (evt) => {
+    formElement.addEventListener("submit", (evt) => {
       evt.preventDefault();
     });
-    
+
     setEventListeners(formElement);
   });
+
+  // Показ ошибки ввода
+  function showInputError(formElement, inputElement, errorMessage) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.add(arr["inputErrorClass"]);
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add(arr["errorClass"]);
+  }
+
+  // Скрытие ошибки ввода
+  function hideInputError(formElement, inputElement) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    inputElement.classList.remove(arr["inputErrorClass"]);
+    errorElement.classList.remove(arr["errorClass"]);
+    errorElement.textContent = "";
+  }
+
+  // Проверка валидности введённых данных
+  function checkInputValidity(formElement, inputElement) {
+    if (!inputElement.validity.valid) {
+      showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+      hideInputError(formElement, inputElement);
+    }
+  }
+
+  // Проверка валидности всех полей формы
+  function checkInvalidInputs(inputsList) {
+    return inputsList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  }
+
+  // Переключатель состояния кнопки отправки формы
+  function toggleButtonState(inputsList, buttonElement) {
+    if (checkInvalidInputs(inputsList)) {
+      buttonElement.classList.add(arr["inactiveButtonClass"]);
+      buttonElement.disabled = true;
+    } else {
+      buttonElement.classList.remove(arr["inactiveButtonClass"]);
+      buttonElement.disabled = false;
+    }
+  }
+
+  // Добавление обработчиков всем полям формы
+  function setEventListeners(formElement) {
+    const inputsList = Array.from(
+      formElement.querySelectorAll(arr["inputSelector"])
+    );
+    const buttonElement = formElement.querySelector(
+      arr["submitButtonSelector"]
+    );
+
+    toggleButtonState(inputsList, buttonElement);
+
+    inputsList.forEach((inputElement) => {
+      inputElement.addEventListener("input", function () {
+        checkInputValidity(formElement, inputElement);
+        toggleButtonState(inputsList, buttonElement);
+      });
+    });
+  }
 }
